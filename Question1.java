61bcd6a839e68e43953a1b2f18dc62f6f1163044
Kayla Afonseca <kayla.afonseca@daviestech.org>
2020-10-02 15:21:30 +0000

    All, to match assignment

--- Question1.java
+++ Question1.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Give me two neumbers on different lines!");
+      double x = scan.nextDouble();
+     double y = scan.nextDouble();
+     System.out.print(y + " " + x);
 
-        /* Write your code here */
 
     }
 }
--- Question2.java
+++ Question2.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 
 class Question2 {
     public static void main(String[] args) {
-
-        /* Write your code here */
-
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Hello, what is your name?");
+      String name = scan.nextLine();
+      System.out.print("Hi " + name + "! How old are you? ");
+      int age = scan.nextInt();
+      System.out.print(name + " is " + age + " years old!" );
     }
 }
